153f4c7701b5b007f2fda26ad12362b2cd50922a
Yaying Liang Li <72949647+yayeetying@users.noreply.github.com>
2024-12-27 00:59:56 -0500

    react week 2 part 3

--- bootcamp/src/App.js
+++ bootcamp/src/App.js
@@ -12,8 +12,26 @@ class App extends React.Component{
     };
   }
 
+  addCard = card => {
+    // Copy old cards, concat new cards, reset cards variable with new set of cards
+    const cards = this.state.cards.slice().concat(card);
+    this.setState({cards});
+  }
+
+  deleteCard = index => {
+    const cards = this.state.cards.slice();
+    // Remove 1 card at index; mutates cards in-place
+    cards.splice(index, 1);
+    this.setState({cards});
+  }
+
   render() {
-    return <CardEditor cards={this.state.cards}/>;
+    // addCard (the function) and cards are props sent to CardEditor
+    return <CardEditor 
+      addCard={this.addCard} 
+      deleteCard={this.deleteCard}
+      cards={this.state.cards}
+    />;
   }
 }
 
--- bootcamp/src/CardEditor.js
+++ bootcamp/src/CardEditor.js
@@ -8,6 +8,16 @@ class CardEditor extends React.Component {
         this.state = {front: '', back: ''};
     }
 
+    addCard = () => {
+        // this.state is a "card" in App.js 'cuz it has the same format as the other cards
+        this.props.addCard(this.state);
+        this.setState({front: '', back: ''});
+    }
+    
+    deleteCard = index => {
+        this.props.deleteCard(index);
+    }
+
     // Change this.state's front or back value in real-time as user types
     handleChange = event => 
         this.setState({ [event.target.name]: event.target.value })
@@ -19,7 +29,7 @@ class CardEditor extends React.Component {
                     <td>{card.front}</td>
                     <td>{card.back}</td>
                     <td>
-                        <button> Delete Card</button>
+                        <button onClick={() => this.deleteCard(index)}> Delete Card</button>
                     </td>
                 </tr>
             );
@@ -54,7 +64,7 @@ class CardEditor extends React.Component {
                     value={this.state.back}
                     onChange={this.handleChange}
                 />
-                <button>Add Card</button>
+                <button onClick={this.addCard}>Add Card</button>
             </div>
         );
     }
