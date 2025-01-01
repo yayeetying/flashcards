a99b9f9586411cff3a08bc058372e3c7e7285b3c
Yaying Liang Li <72949647+yayeetying@users.noreply.github.com>
2024-12-31 23:35:26 -0500

    homepage displays all flashcards

--- bootcamp/src/Homepage.js
+++ bootcamp/src/Homepage.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { firebaseConnect, isLoaded } from "react-redux-firebase";
+import { connect } from 'react-redux';
+import { compose } from "redux";
 
 class Homepage extends React.Component {
     render() {
+        const { urls, decks } = this.props;
+
+        if (!isLoaded(decks)) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
                 <h2> Homepage to Flashcards </h2>
-                <hr/> <br />
+                <hr /> <br />
                 <Link to='/editor'>Create a new card deck!</Link>
-                <br/> 
+                <br />
                 <h3> Flashcards </h3>
-                <Link to='/viewer'>Go to Card Viewer</Link>
+                <ul>
+                    {urls.map((deckID) => (
+                        <li key={deckID}>
+                            <Link to={`/viewer/${deckID}`}>
+                                {decks[deckID].name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
 }
 
-export default Homepage;
+const mapStatetoProp = (state) => {
+    // All of the Objects of homepage
+    const decks = state.firebase.data.homepage || {};
+    // Just the urls of those Objects
+    const urls = Object.keys(decks);
+    return { urls, decks };
+};
+
+export default compose(
+    withRouter,
+    firebaseConnect(() => {
+        return [{ path: '/homepage/' }]; // Get the information from Firebase /homepage
+    }),
+    connect(mapStatetoProp)
+)(Homepage);
